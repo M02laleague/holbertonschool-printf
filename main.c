15bdba6351149f7fb69889b657acd9826853b639
zyclamenace <gemicieneshakki@gmail.com>
2024-07-22 15:41:41 +0200

    Creation main.c avec les bases include, commentaire vide, fonction _printf

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - Display characters or variables like the standard printf function in language C.
+ * 
+ * Return:
+*/
+
+int _printf(const char *format, ...)
+{
+
+}
