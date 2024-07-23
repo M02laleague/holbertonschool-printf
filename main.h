c1ff2b718f2ecad20ba7764b708d6ec42e57176c
zyclamenace <gemicieneshakki@gmail.com>
2024-07-23 15:32:58 +0200

    _putchar in main.h

--- main.h
+++ main.h
@@ -4,8 +4,20 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include "GetFunctionFormat.c"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
 
 int _printf(const char *format, ...);
-int _putchar(char c);
 
 #endif
