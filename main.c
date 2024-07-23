4d516565c9a2ec8d0fc40912e62d221aecd6cbd7
zyclamenace <gemicieneshakki@gmail.com>
2024-07-23 15:32:42 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,5 +8,39 @@
 
 int _printf(const char *format, ...)
 {
+	int i;
+    va_list args;
+    va_start(args, format);
 
+
+    for (i = 0; format[i] != '\0'; i++)
+	{
+        if (format[i] == '%') 
+		{
+            i++; 
+            if (format[i] == 'c')
+			{
+                char c = va_arg(args, int);
+                _putchar(c);
+            }
+			else if (format[i] == 's')
+			{
+                char *s = va_arg(args, char *);
+                while (*s) {
+                    _putchar(*s++);
+                }
+            }
+			else if (format[i] == '%') 
+			{
+                _putchar('%');
+            }
+        } 
+		else 
+		{
+            _putchar(format[i]);
+        }
+    }
+
+    va_end(args);
+	return(0);
 }
