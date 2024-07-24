4931b8ce325707164fb50c085123977eda8d9d89
zyclamenace <gemicieneshakki@gmail.com>
2024-07-24 16:40:00 +0200

    GetFunctionFormat.c creating a struct and every format we will use

--- GetFunctionFormat.c
+++ GetFunctionFormat.c
@@ -6,3 +6,24 @@
  * 
  * Return:
 */
+
+typedef struct {
+    char format;
+    int (*func)(va_list);
+} format_t;
+
+format_t formats[] =
+{
+    {'c', print_char},
+    {'s', print_string},
+    {'i', print_int},
+    {'d', print_int},
+    {'f', print_float},
+    {'o', print_octal},
+    {'x', print_hexa},
+    {'X', print_caps_hexa},
+    {'u', print_ui},
+    {'p', print_addr},
+   
+    {0, NULL}
+};
