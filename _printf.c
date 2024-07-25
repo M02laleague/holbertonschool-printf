cd6442bab876ff47ff72267d8e87c8d736ef804f
zyclamenace <gemicieneshakki@gmail.com>
2024-07-25 14:55:06 +0200

    trying to fix print int

--- _printf.c
+++ _printf.c
@@ -10,43 +10,41 @@
 */
 int _printf(const char *format, ...)
 {
-	int i;
-	int count;
+	int i, count = 0;
 	va_list args;
 
 	va_start(args, format);
+	if (format == NULL)
+		return (-1);
 	for (i = 0; format[i] != '\0'; i++)
 	{
+		if (format[i] == '\0')
+		return (-1);
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == 'c')
-			{
-			count +=  print_char(args);
-			}
-			else if (format[i] == 's')
-			{
-			count += print_string(args);
-			}
-			else if (format[i] == 'i')
-			{
-			count +=  print_int(args);
-			}
-			else if (format[i] == 'd')
-			{
-			count +=  print_int(args);
-			}
-			else if (format[i] == '%')
-			{
-				_putchar('%');
-			}
-		}
-		else
+				if (format[i] == 'c')
+				{
+				count +=  print_char(args);
+				}
+				else if (format[i] == 's')
+				{
+				count += print_string(args);
+				}
+				else if (format[i] == 'i' || format[i] == 'd')
+				{
+				count +=  print_int(args);
+				}
+				else if (format[i] == '%')
+				{
+					_putchar('%');
+				}
+		} else
 		{
 			_putchar(format[i]);
+			count += 2;
 		}
 	}
-
 	va_end(args);
 	return (count);
 }
