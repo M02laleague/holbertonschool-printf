25247515dc9547a53f85e0552d19128eecb9dd86
zyclamenace <gemicieneshakki@gmail.com>
2024-07-25 15:11:08 +0200

    Maybe final _printf.c push

--- _printf.c
+++ _printf.c
@@ -13,36 +13,34 @@ int _printf(const char *format, ...)
 	int i, count = 0;
 	va_list args;
 
-	va_start(args, format);
 	if (format == NULL)
 		return (-1);
+
+	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '\0')
-		return (-1);
 		if (format[i] == '%')
-		{
-			i++;
-				if (format[i] == 'c')
-				{
-				count +=  print_char(args);
-				}
-				else if (format[i] == 's')
-				{
+		{ i++;
+			if (format[i] == '\0')
+				return (-1);
+			if (format[i] == 'c')
+				count += print_char(args);
+			else if (format[i] == 's')
 				count += print_string(args);
-				}
-				else if (format[i] == 'i' || format[i] == 'd')
-				{
-				count +=  print_int(args);
-				}
-				else if (format[i] == '%')
-				{
-					_putchar('%');
-				}
+			else if (format[i] == 'i' || format[i] == 'd')
+				count += print_int(args);
+			else if (format[i] == '%')
+			{ _putchar('%');
+				count++;
+			}
+			else
+			{ _putchar('%');
+				_putchar(format[i]);
+				count += 2;
+			}
 		} else
-		{
-			_putchar(format[i]);
-			count += 2;
+		{ _putchar(format[i]);
+			count++;
 		}
 	}
 	va_end(args);
