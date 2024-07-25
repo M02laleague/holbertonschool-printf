19f019413db33d953c190fe501dfb7ca3164391e
zyclamenace <gemicieneshakki@gmail.com>
2024-07-25 14:29:30 +0200

    updating and fixing

--- main.h
+++ main.h
@@ -3,13 +3,13 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
-#include "FunctionFormat/print_char.c"
 #include <unistd.h>
 
 int _printf(const char *format, ...);
-int print_int(va_list args);
 int _putchar(char c);
-int print_int(va_list args);
+int print_char(va_list args);
 int print_string(va_list args);
+int print_int(va_list args);
+int print_int_recursive(int n);
 
 #endif
