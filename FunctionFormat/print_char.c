9eee44e56f1fbe17fd083dc451b96255f2f13c86
zyclamenace <gemicieneshakki@gmail.com>
2024-07-25 11:40:43 +0200

    Fix print_char.c

--- FunctionFormat/print_char.c
+++ FunctionFormat/print_char.c
@@ -1,14 +1,14 @@
-#include "main.h"
+#include "../main.h"
 /**
  * print_char - prints integer
  * @args: argument to print
  * Return: number of characters printed
  */
-int printf_char(va_list args)
+int print_char(va_list args)
 {
 	char s;
 
-	s = va_arg(val, int);
+	s = va_arg(args, int);
 	_putchar(s);
 	return (1);
 }
